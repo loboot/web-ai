6d37529fabbac6fe279efdf9167b569e5c9afe19
aweii <a_weii@163.com>
2024-11-06 01:50:16 +0800

    chore:  env

--- admin/src/api/modules/config.ts
+++ admin/src/api/modules/config.ts
@@ -1,15 +1,17 @@
-import api from '../index'
+import { KeyItem } from './../../../../service/src/common/utils/selectKeyWithWeight';
+import api from '../index';
 
 interface KeyValue {
-  configKey: string
-  configVal: any
+  configKey: string;
+  configVal: any;
 }
 
 export default {
   queryAllConfig: () => api.get('config/queryAll'),
   queryGptKeys: () => api.get('config/queryGptKeys'),
-  setGptKeys: data => api.post('config/setGptKeys', data),
-  queryConfig: data => api.post('config/query', data),
+  setGptKeys: (data: { configs: KeyItem }) =>
+    api.post('config/setGptKeys', data),
+  queryConfig: (data: { keys: string[] }) => api.post('config/query', data),
   copyright: () => api.get('config/copyright'),
   setConfig: (data: { settings: KeyValue[] }) => api.post('config/set', data),
-}
+};
