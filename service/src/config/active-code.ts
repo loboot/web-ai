bdc2a09747245fda1ba3e91e5a9bbc8d0ec892ac
aweii <a_weii@163.com>
2025-01-11 14:02:13 +0800

    feat: 激活码url

--- service/src/config/active-code.ts
+++ service/src/config/active-code.ts
@@ -2,6 +2,6 @@ import { join } from 'path';
 import { ConnectionOptions } from 'typeorm';
 
 const config = {
-  baseUrl: 'http://106.14.240.89',
+  baseUrl: 'http://106.14.240.89:3000',
 };
 export default config;
